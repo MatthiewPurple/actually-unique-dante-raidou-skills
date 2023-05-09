d0805c8169546ed9cfcbe4bfd84fbbbf239eac4a
Matthiew Purple <matthiewpurple@hotmail.com>
2023-05-09 16:51:31 +0200

    better now

--- DanteRaidouBuff.cs
+++ DanteRaidouBuff.cs
@@ -1,6 +1,7 @@
 ﻿using MelonLoader;
 using Il2Cpp;
 using dante_raidou_buff;
+using HarmonyLib;
 
 [assembly: MelonInfo(typeof(DanteRaidouBuff), "Dante/Raidou buff", "1.0.0", "Matthiew Purple")]
 [assembly: MelonGame("アトラス", "smt3hd")]
@@ -8,29 +9,34 @@ using dante_raidou_buff;
 namespace dante_raidou_buff;
 public class DanteRaidouBuff : MelonMod
 {
+    [HarmonyPatch(typeof(datSkillHelp_msg), nameof(datSkillHelp_msg.Get))]
+    private class Patch
+    {
+        // Token: 0x06000050 RID: 80 RVA: 0x00003E98 File Offset: 0x00002098
+        public static void Postfix(ref int id, ref string __result)
+        {
+            switch (id)
+            {
+                case 265:
+                    __result = "Greatly lower all foes' \nDefense. \nUser: Slight MP recovery.";
+                    break;
+                case 274:
+                    __result = "Negates -nda effects \nand raises the party's \nPhysical/Magical Attack.";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
     // When launching the game
     public override void OnInitializeMelon()
     {
-        byte danteID = 192;
+        // Buffs Raptor Guardian
+        datNormalSkill.tbl[274].hojotype = 262149; // Dekunda + Tarukaja + Makakaja
+        datNormalSkill.tbl[274].cost = 50;
 
-        // Adds Avenge at level 85
-        tblSkill.fclSkillTbl[danteID].Event[12].TargetLevel = 85;
-        tblSkill.fclSkillTbl[danteID].Event[12].Param = 307;
-        tblSkill.fclSkillTbl[danteID].Event[12].Type = 1;
-
-        // Adds Might at level 86
-        tblSkill.fclSkillTbl[danteID].Event[13].TargetLevel = 86;
-        tblSkill.fclSkillTbl[danteID].Event[13].Param = 299;
-        tblSkill.fclSkillTbl[danteID].Event[13].Type = 1;
-
-        // Adds Focus at level 87
-        tblSkill.fclSkillTbl[danteID].Event[14].TargetLevel = 87;
-        tblSkill.fclSkillTbl[danteID].Event[14].Param = 224;
-        tblSkill.fclSkillTbl[danteID].Event[14].Type = 1;
-
-        // Adds Debilitate at level 88
-        tblSkill.fclSkillTbl[danteID].Event[14].TargetLevel = 88;
-        tblSkill.fclSkillTbl[danteID].Event[14].Param = 206;
-        tblSkill.fclSkillTbl[danteID].Event[14].Type = 1;
+        // Buffs Provoke
+        datNormalSkill.tbl[265].hojotype = 128; // Rakunda + Rakunda
     }
 }
